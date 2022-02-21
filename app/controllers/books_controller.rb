68789d712901cce2f7089f72aeec96d95ddefa13
TetsuoMiyake <tmky1919mnstwebcamp@gmail.com>
2022-02-21 06:05:22 +0000

    create_scuccess_flash

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
@@ -13,10 +14,24 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path
+    flash[:notice] = 'Book was successfully created.'
+    redirect_to book_path(book)
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
